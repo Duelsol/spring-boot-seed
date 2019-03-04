0cae843fb7a4c763237e0a46294883907c826a63
Duelsol <duelsol@sina.com>
2019-03-04 21:16:56 +0800

    增加默认线程池配置。

--- src/main/java/me/duelsol/springbootseed/SpringBootSeedApplication.java
+++ src/main/java/me/duelsol/springbootseed/SpringBootSeedApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableCaching
 @EnableJpaAuditing
+@EnableAsync
+@EnableScheduling
 public class SpringBootSeedApplication {
 
     public static void main(String[] args) {
