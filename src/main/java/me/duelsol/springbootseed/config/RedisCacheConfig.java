96e7a6ca639807c90e1122412ebc38a059962cea
Duelsol <duelsol@sina.com>
2020-06-11 15:51:38 +0800

    增加Redis缓存报错处理。

--- src/main/java/me/duelsol/springbootseed/config/RedisCacheConfig.java
+++ src/main/java/me/duelsol/springbootseed/config/RedisCacheConfig.java
@@ -3,8 +3,11 @@ package me.duelsol.springbootseed.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -19,6 +22,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 /**
  * @author 冯奕骅
  */
+@Slf4j
 @Configuration
 public class RedisCacheConfig extends CachingConfigurerSupport {
 
@@ -47,6 +51,32 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
         redisTemplate.setHashValueSerializer(stringSerializer);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Bean
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.error("Cache get error, Redis key:" + cache.getName() + ":" + key, e);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.error("Cache put error, Redis key:" + cache.getName() + ":" + key, e);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key) {
+                log.error("Cache evict error, Redis key:" + cache.getName() + ":" + key, e);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                log.error("Cache clear error, Redis key:" + cache.getName(), e);
+            }
+        };
     }
 
 }
